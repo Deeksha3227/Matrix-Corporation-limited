0ab286874fac0c184c5df8b30bf5d4b87114848a
Deeksha <deeksha@example.com>
2026-07-02 18:22:37 +0530

    Setup GitHub Pages deployment with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
+  return {    base: '/Matrix-Corporation-limited/',    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
